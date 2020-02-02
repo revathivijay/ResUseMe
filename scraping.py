8e23c7985d1a99e650262ae198e4cb0420180e0b
revathivijay <34963337+revathivijay@users.noreply.github.com>
2020-02-02 06:34:01 +0530

    fixed upload saving to pdf issue

--- scraping.py
+++ scraping.py
@@ -1,25 +1,30 @@
-
+from fpdf import FPDF
 import names
+from time import sleep
 
+from cleanDataset import cleanResume
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import html2text
-
 import bs4
-CHROME_PATH = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-CHROMEDRIVER_PATH = 'C:/Program Files/chromedriver'
+
+CHROME_PATH = '/Users/revathi/Library/Application Support/Google/Chrome/Default'
+CHROMEDRIVER_PATH = '/Users/revathi/chromedriver'
 WINDOW_SIZE = "1920,1080"
 
+#chrome requirements
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.binary_location = CHROME_PATH
 
+#pdf requirements
+pdf = FPDF()
+
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                          chrome_options=chrome_options
-                         )
-driver = webdriver.Chrome('C:/Program Files/chromedriver')
+                          chrome_options=chrome_options)
+# driver = webdriver.Chrome('C:/Program Files/chromedriver')
 h = html2text.HTML2Text()
 def scraper(job_title):
     #job_title="fashion designer" #hardcoded for now
@@ -34,13 +39,20 @@ def scraper(job_title):
             for k in job_split:
                 if(k.lower() in link['href']):
                     links.append('https://www.livecareer.com'+link['href'])
-                    print('https://www.livecareer.com'+link['href'])
-
+                    # print('https://www.livecareer.com'+link['href'])
     for link in links:
         driver.get(link)
         elem = driver.find_element_by_id('document')
         resume = h.handle(elem.get_attribute('innerHTML')).encode('utf-8')
-        #insert saving code here
+        resume = resume.decode('utf-8')
+        resume = cleanResume(resume)
+
+        pdf.add_page()
+        pdf.set_font("Arial",size=12)
+        pdf.cell(200, 10, txt=resume, ln=1)
+
+        name = names.get_full_name()
+        pdf.output('resumes/' + name +'.pdf')
 
 
-scraper('Python Developer')
+# scraper('Python Developer')
